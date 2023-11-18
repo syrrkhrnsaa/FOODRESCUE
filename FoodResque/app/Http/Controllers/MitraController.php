22791c09618709f14e7d6cb5ec36dd0653d3d7bb
syira <syira.khoerunisa.tif22@polban.ac.id>
2023-11-19 06:58:14 +0700

    membuat yajra dan 1000 table

--- FoodResque/app/Http/Controllers/MitraController.php
+++ FoodResque/app/Http/Controllers/MitraController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Mitra;
 use DataTables;
-//use Yajra\DataTables\Facades\DataTables;
 
 class MitraController extends Controller
 {
@@ -14,21 +13,27 @@ class MitraController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request)
+    public function index()
     {
-        if ($request->ajax()) {
-            $data = Mitra::latest()->get();
-            return DataTables::of($data)
-                ->addColumn('action', function ($row) {
-                    $btn = '<a href="mitra/' . $row->id . '/edit" class="edit btn btn-primary btn-sm">Edit</a>';
-                    $btn .= '&nbsp;<button type="button" data-id="' . $row->id . '" class="delete btn btn-danger btn-sm">Delete</button>';
+        return view('mitra.index');
+    }
+
+    public function mitraData()
+    {
+        $mitra = Mitra::select(['mitra_id','username', 'nama_mitra', 'alamat', 'no_telp']);
+            return DataTables::of($mitra)
+                ->addColumn('action', function ($mitra) {
+                    $btn = '<a href="' . route('mitra.show', $mitra->mitra_id) . '" class="btn btn-info">View</a>';
+                    $btn .= ' <a href="' . route('mitra.edit', $mitra->mitra_id) . '" class="btn btn-primary">Edit</a>';
+                    $btn .= ' <form action="' . route('mitra.destroy', $mitra->mitra_id) . '" method="POST" style="display: inline-block;">';
+                    $btn .=  csrf_field();
+                    $btn .=  method_field('DELETE');
+                    $btn .= ' <button type="submit" class="btn btn-danger" onclick="return confirm(\'Are you sure you want to delete this item?\')">Delete</button>';
+                    $btn .= ' </form>';
                     return $btn;
                 })
                 ->rawColumns(['action'])
                 ->make(true);
-        }
-
-        return view('mitra.index');
     }
 
     /**
