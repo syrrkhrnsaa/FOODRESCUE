2db33ab1624ee924a6e8339854c2d930f3b72806
Alya_Angraini <116321705+aruya2707@users.noreply.github.com>
2023-11-23 10:24:00 +0700

    yajra

--- FoodResque/app/Http/Controllers/DonaturController.php
+++ FoodResque/app/Http/Controllers/DonaturController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use DataTables;
+use Yajra\DataTables\DataTables;
 use Illuminate\Http\Request;
 use App\Models\Donatur;
 
--- FoodResque/app/Http/Controllers/MitraController.php
+++ FoodResque/app/Http/Controllers/MitraController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Mitra;
-use DataTables;
+use Yajra\DataTables\DataTables;
 
 class MitraController extends Controller
 {
